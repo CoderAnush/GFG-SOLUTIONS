8ba97950f9d594531f2625fa08f558927311e757
ANUSH RAMESH <anushramesh2023@gmail.com>
2026-02-10 10:38:56 +0530

    Updated solution

--- 1. Indexes of Subarray Sum/subarray.c
+++ 1. Indexes of Subarray Sum/subarray.c
@@ -1,16 +1,24 @@
+#include <stdio.h>
+
 int subarraySum(int* arr, int n, int target, int* res) {
-    // code here
-    for (int i=0;i<n;i++){ 
-    int sum=0;
-        for (int j=i;j<n;j++){
-            sum=sum+arr[j];
-            
-        if (sum==target){
-            res[0] = i+1;   // start index
-            res[1] = j+1;   // end index
-            return 1;
+    int start = 0;
+    long long curr_sum = 0;
+
+    for (int end = 0; end < n; end++) {
+        curr_sum += arr[end];
+
+        while (curr_sum > target && start < end) {
+            curr_sum -= arr[start];
+            start++;
         }
+
+        if (curr_sum == target) {
+            res[0] = start + 1; // 1-based index
+            res[1] = end + 1;
+            return 1; // found
         }
-}
-return -1;
+    }
+
+    res[0] = -1; // not found
+    return 0;
 }
